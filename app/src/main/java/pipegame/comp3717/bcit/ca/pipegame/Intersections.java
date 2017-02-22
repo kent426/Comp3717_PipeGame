9e68ec9d6b0640ab1ea5f564e866ca311fc11abe
Kent Huang <kenttttt0@gmail.com>
2017-02-21 20:30:33 -0800

    add midpoints(valves) between intersections

---
+++ app/src/main/java/pipegame/comp3717/bcit/ca/pipegame/Intersections.java
@@ -0,0 +1,88 @@
+package pipegame.comp3717.bcit.ca.pipegame;
+
+import android.app.Activity;
+import android.content.res.Resources;
+import android.graphics.Color;
+import android.util.Log;
+
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.PolylineOptions;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+/**
+ * Created by Kent on 2017-02-20.
+ */
+
+public class Intersections {
+    private ArrayList<String[]> allconnectedPs;
+    private ArrayList<LatLng> points;
+    Activity act;
+
+    public Intersections(Activity a) {
+        allconnectedPs = new ArrayList<String[]>();
+        points = new ArrayList<LatLng>();
+        act = a;
+    }
+
+
+
+
+
+    public void readPoints() {
+        Scanner s = new Scanner(act.getResources().openRawResource(R.raw.pathpoints));
+
+
+        while (s.hasNext()) {
+            String line = s.next();
+            String[] pieces = line.split(",");
+            points.add(new LatLng((Double.parseDouble(pieces[1]) + Double.parseDouble(pieces[3]))/2,(Double.parseDouble(pieces[0]) + Double.parseDouble(pieces[2]))/2));
+            allconnectedPs.add(pieces);
+
+
+
+
+        }
+        s.close();
+    }
+
+
+
+    public ArrayList<PolylineOptions> getPolylineOptions() {
+        ArrayList<PolylineOptions> allPolylines = new ArrayList<PolylineOptions>();
+        int count = 1;
+        int col = Color.BLACK;
+
+        for(String[] pieces: allconnectedPs) {
+            switch((count++)%5) {
+                case 0: col = Color.BLACK; break;
+                case 1: col = Color.RED;break;
+                case 2: col = Color.BLUE;break;
+                case 3: col =Color.GREEN;break;
+                case 4: col = Color.MAGENTA;break;
+
+
+            }
+            allPolylines.add(new PolylineOptions()
+                    .add(new LatLng(Double.parseDouble(pieces[1]), Double.parseDouble(pieces[0])), new LatLng(Double.parseDouble(pieces[3]), Double.parseDouble(pieces[2])))
+                    .width(5)
+                    .color(col));
+            /*Log.d("points",pieces[0]+pieces[1]+pieces[2]+pieces[3]);*/
+        }
+        return allPolylines;
+    }
+
+    public ArrayList<LatLng> getPoints() {
+        return points;
+    }
+
+
+
+
+
+
+
+}
--- app/src/main/java/pipegame/comp3717/bcit/ca/pipegame/Map.java
+++ app/src/main/java/pipegame/comp3717/bcit/ca/pipegame/Map.java
@@ -1,13 +1,10 @@
 package pipegame.comp3717.bcit.ca.pipegame;
 
-import android.graphics.Color;
-import android.net.Uri;
-import android.os.Bundle;
+import android.app.Activity;
+import android.os.AsyncTask;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
-import com.google.android.gms.appindexing.Action;
-import com.google.android.gms.appindexing.AppIndex;
-import com.google.android.gms.appindexing.Thing;
-import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -17,83 +14,117 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.LatLngBounds;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.maps.model.PolylineOptions;
-import com.google.maps.android.MarkerManager;
 import com.google.maps.android.kml.KmlContainer;
 import com.google.maps.android.kml.KmlGeometry;
 import com.google.maps.android.kml.KmlLayer;
 import com.google.maps.android.kml.KmlPlacemark;
 import com.google.maps.android.kml.KmlPolygon;
 
-import android.os.Parcelable;
-import android.support.annotation.Nullable;
-import android.support.v7.app.ActionBar;
-import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
-
-import org.json.JSONException;
-import org.json.JSONObject;
 import org.xmlpull.v1.XmlPullParserException;
 
 import java.io.IOException;
-import java.io.InputStream;
 import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Collections;
 import java.util.List;
-import java.util.Scanner;
 
-public class GameActivity extends AppCompatActivity implements OnMapReadyCallback {
+/**
+ * Created by Kent on 2017-02-21.
+ */
+
+public class Map extends AsyncTask<GoogleMap, Integer, GoogleMap>   {
+
+    private GameActivity activity;
 
     private GoogleMap map;
+    Intersections intersections;
+    ArrayList<PolylineOptions> allConnectedPoints;
+
+    private KmlLayer kmlIntersectionsLayer;
+    private KmlLayer kmlNeighbourhood_areas;
+    private ArrayList<LatLng> points;
+    static  private List<LatLngBounds> areas;
+    //points in different level
+    static private List<ArrayList<LatLng>> pointSets;
     //the levels:from 0 to 14, used for filtering the points
     private int levelarea;
-    /**
-     * ATTENTION: This was auto-generated to implement the App Indexing API.
-     * See https://g.co/AppIndexing/AndroidStudio for more information.
-     */
-    private GoogleApiClient client;
+
+    public Map(GameActivity a) {
+        activity = a;
+        intersections = new Intersections(a);
 
 
-
-
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_game);
-
-        levelarea = this.getIntent().getIntExtra("level", 0) - 1;
-
-
-        SupportMapFragment mapFragment =
-                (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
-        mapFragment.getMapAsync(this);
-        /*getSupportActionBar().setDisplayHomeAsUpEnabled(true);*/
-        // ATTENTION: This was auto-generated to implement the App Indexing API.
-        // See https://g.co/AppIndexing/AndroidStudio for more information.
-        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
 
+    protected void onPreExecute() {
+        super.onPreExecute();
+        retrieveFileFromResource();
+        map = activity.getmap();
+        displayPointsAndArea(activity.getLevelarea(),areas,pointSets);
 
 
-    @Override
-    public void onMapReady(GoogleMap googleMap) {
-        map = googleMap;
-
-/*        levelarea = this.getIntent().getIntExtra("level", -1) - 1;
-        displayPointsAndArea(levelarea, areas, pointSets);*/
-
-
-        map.addMarker(new MarkerOptions().position(new LatLng(49.215826, -122.929966)).title("Marker"));
-        map.addMarker(new MarkerOptions().position(new LatLng(49.215826, -122.929436)).title("Marker"));
-
-
-
-        new Map(this).execute(map);
     }
 
+    @Override
+    protected GoogleMap doInBackground(GoogleMap... g) {
+        intersections.readPoints();
+        allConnectedPoints = intersections.getPolylineOptions();
+        points = intersections.getPoints();
 
+
+
+        return g[0];
+
+    }
+
+    @Override
+    protected void onProgressUpdate(Integer... values) {
+        super.onProgressUpdate(values);
+    }
+
+    @Override
+    protected void onPostExecute(GoogleMap g) {
+
+        super.onPostExecute(g);
+        for(PolylineOptions p: allConnectedPoints) {
+            g.addPolyline(p);
+        }
+
+        for (LatLng p : points) {
+            map.addMarker(new MarkerOptions()
+                    .position(p)
+                    .anchor(0.5f,0.5f)
+                    .icon(BitmapDescriptorFactory.fromResource(R.raw.inters)));
+            Log.d("show", "onPostExecute: ");
+        }
+
+
+    }
+
+    private void retrieveFileFromResource() {
+        try {
+            /*kmlIntersectionsLayer = new KmlLayer(map, R.raw.intersections,this.activity.getApplicationContext());*/
+            kmlNeighbourhood_areas = new KmlLayer(map, R.raw.areas, this.activity.getApplicationContext());
+            /*points = new ArrayList<LatLng>();*/
+            areas = new ArrayList<LatLngBounds>();
+
+
+            /*points = getPoints(kmlIntersectionsLayer.getContainers());*/
+            areas = getPolygons(kmlNeighbourhood_areas.getContainers());
+
+
+
+
+            /*pointSets = sortPointsToAreas(points, areas);*/
+
+            Log.d("gamelevl", "now" + levelarea);
+            /*Log.d("pointsse", pointSets.toString());*/
+            /*kmlIntersectionsLayer.addLayerToMap();*/
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (XmlPullParserException e) {
+            e.printStackTrace();
+        }
+    }
 
     private List<LatLng> getPoints(final Iterable<KmlContainer> containers) {
         List<LatLng> ps = new ArrayList<LatLng>();
@@ -165,66 +196,19 @@ public class GameActivity extends AppCompatActivity implements OnMapReadyCallbac
     }
 
     private void displayPointsAndArea(int level, List<LatLngBounds> areas, List<ArrayList<LatLng>> pointSets) {
-        int width = getResources().getDisplayMetrics().widthPixels;
-        int height = getResources().getDisplayMetrics().heightPixels;
+        int width = this.activity.getResources().getDisplayMetrics().widthPixels;
+        int height = this.activity.getResources().getDisplayMetrics().heightPixels;
         map.moveCamera(CameraUpdateFactory.newLatLngBounds(areas.get(level), width, height, 1));
 
 
 
-        for (LatLng p : pointSets.get(level)) {
+/*        for (LatLng p : pointSets.get(level)) {
             map.addMarker(new MarkerOptions()
                     .position(p)
                     .icon(BitmapDescriptorFactory.fromResource(R.raw.inters)));
 
-        }
+        }*/
 
 
-    }
-
-    /**
-     * ATTENTION: This was auto-generated to implement the App Indexing API.
-     * See https://g.co/AppIndexing/AndroidStudio for more information.
-     */
-    public Action getIndexApiAction() {
-        Thing object = new Thing.Builder()
-                .setName("Game Page") // TODO: Define a title for the content shown.
-                // TODO: Make sure this auto-generated URL is correct.
-                .setUrl(Uri.parse("http://[ENTER-YOUR-URL-HERE]"))
-                .build();
-        return new Action.Builder(Action.TYPE_VIEW)
-                .setObject(object)
-                .setActionStatus(Action.STATUS_TYPE_COMPLETED)
-                .build();
-    }
-
-
-    @Override
-    public void onStart()  {
-        super.onStart();
-
-        // ATTENTION: This was auto-generated to implement the App Indexing API.
-        // See https://g.co/AppIndexing/AndroidStudio for more information.
-        client.connect();
-        AppIndex.AppIndexApi.start(client, getIndexApiAction());
-
-
-    }
-
-    @Override
-    public void onStop() {
-        super.onStop();
-
-        // ATTENTION: This was auto-generated to implement the App Indexing API.
-        // See https://g.co/AppIndexing/AndroidStudio for more information.
-        AppIndex.AppIndexApi.end(client, getIndexApiAction());
-        client.disconnect();
-    }
-
-    public int getLevelarea() {
-        return levelarea;
-    }
-
-    public GoogleMap getmap() {
-        return map;
     }
 }
